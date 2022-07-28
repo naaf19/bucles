89138b4e922e8aa90f5039f916e45847276503cd
Nelson Acosta <nelsucho@gmail.com>
2022-07-28 20:25:22 +0200

    prueba conflicto actualizando nelson

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Que te pasa Nelson");
+        System.out.println("Hello!");
 
 
         //esto es una prueba de push
